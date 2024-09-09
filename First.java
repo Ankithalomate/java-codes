22dc2540b4b3662066e894cfff148c72cc67f6b3
Ankitha <ankithalomate@gmail.com>
2024-09-09 16:50:26 +0530

    removed some lines

--- First.java
+++ First.java
@@ -1,7 +1,5 @@
 public class First{
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Basic Learning2");
-        System.out.println("Basic Learning2");
     }
 }
