d3bf530e4bdad9e87a36c1769992dd7ec3ba8c3e
Ankitha <ankithalomate@gmail.com>
2024-09-09 19:38:55 +0530

    operators

--- Codes.java
+++ Codes.java
@@ -1,5 +1,8 @@
 public class Codes {
     public static void main(String[] args) {
-        System.out.println("data types");
+        int a=6;
+        int b=7;
+        boolean result= a>b;
+        System.out.println(result);
     }
 }
